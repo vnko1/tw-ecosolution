e464de8155b2c0f4f51a61e089bcf4ac6b5bd2fc
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-12-12 21:41:44 +0200

    feat: this add localStorage form field setter

--- src/sections/ContactUs/components/ContactForm/ContactForm.tsx
+++ src/sections/ContactUs/components/ContactForm/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { Form, Formik, FormikHelpers } from "formik";
 
 import { IconEnum } from "@/src/types";
@@ -6,13 +6,8 @@ import { UIButton, TextField } from "@/src/components";
 import { ContactsFormValue } from "./ContactForm.type";
 import { validationSchema } from "./validationSchema";
 import styles from "./ContactForm.module.scss";
+import { getValuesFromLS, removeDataFromLS } from "@/src/utils";
 
-const initialValues: ContactsFormValue = {
-  fullName: "",
-  email: "",
-  phone: "",
-  message: "",
-};
 const formsData = {
   fullName: { label: "* Full name:", placeholder: "John Rosie" },
   email: { label: "* E-mail:", placeholder: "johnrosie@gmail.com" },
@@ -20,48 +15,57 @@ const formsData = {
   message: { label: "Message:", placeholder: "Your message" },
 };
 
+const initialValues = {
+  fullName: "",
+  email: "",
+  phone: "",
+  message: "",
+};
+
 const keyValues = Object.keys(initialValues);
 
 const ContactForm: FC = () => {
+  const [values] = useState(
+    getValuesFromLS<ContactsFormValue>(
+      initialValues,
+      "fullName",
+      "email",
+      "phone",
+      "message"
+    )
+  );
   const [showUMessage, setShowUMessage] = useState(false);
+
   const onHandleSubmit = (
     values: ContactsFormValue,
     { resetForm }: FormikHelpers<ContactsFormValue>
   ) => {
-    const keys = Object.keys(values);
-
-    const trimmedValue = keys.reduce((acc, el) => {
-      return { [el]: values[el as keyof ContactsFormValue].trim(), ...acc };
-    }, {});
-
-    console.log(
-      "🚀 ~ file: ContactForm.tsx:40 ~ trimmedValue ~ trimmedValue:",
-      trimmedValue
+    const trimmedValue = Object.keys(values).reduce(
+      (acc, el) => ({
+        [el]: values[el as keyof ContactsFormValue].trim(),
+        ...acc,
+      }),
+      {}
     );
+
+    console.log(trimmedValue);
     setShowUMessage(true);
-    resetForm();
+    removeDataFromLS("fullName", "email", "phone", "message");
+    resetForm({ values: initialValues });
+    setTimeout(() => {
+      setShowUMessage(false);
+    }, 2000);
   };
-
-  useEffect(() => {
-    let t: number;
-    if (showUMessage) {
-      t = setTimeout(() => {
-        setShowUMessage(false);
-      }, 2000);
-    }
-
-    return () => clearTimeout(t);
-  }, [showUMessage]);
 
   return (
     <div className={styles["contact-form"]}>
-      {
-        <Formik
-          initialValues={initialValues}
-          onSubmit={onHandleSubmit}
-          validationSchema={validationSchema}
-        >
-          {({ isValid, dirty, values }) => (
+      <Formik
+        initialValues={values}
+        onSubmit={onHandleSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ isValid, values }) => {
+          return (
             <Form className={styles["form"]}>
               <div className={styles["form__field"]}>
                 {keyValues.map((key, i) => {
@@ -99,19 +103,19 @@ const ContactForm: FC = () => {
                   icon={IconEnum.ARROW}
                   iconClassNames={styles["icon"]}
                   iconSize={16}
-                  disabled={!isValid || !dirty}
+                  disabled={!isValid}
                   classNames={styles["button"]}
                 >
                   Send
                 </UIButton>
                 {showUMessage ? (
-                  <p className={styles["message"]}>Your message is sent</p>
+                  <p className={styles["message"]}>Your message was sent</p>
                 ) : null}
               </div>
             </Form>
-          )}
-        </Formik>
-      }
+          );
+        }}
+      </Formik>
     </div>
   );
 };
--- src/utils/localStorage/localStorage.ts
+++ src/utils/localStorage/localStorage.ts
@@ -3,5 +3,13 @@ export const setDataToLS = (key: string, value: number | string) =>
 
 export const getDataFromLS = (key: string) => localStorage.getItem(key);
 
+export const getValuesFromLS = <T extends object>(obj: T, ...args: string[]) =>
+  args.reduce((acc, el) => {
+    if (getDataFromLS(el)) {
+      return { ...acc, [el]: JSON.parse(getDataFromLS(el) || "") };
+    }
+    return { ...acc, [el]: "" };
+  }, obj);
+
 export const removeDataFromLS = (...args: string[]) =>
   args.forEach((i) => localStorage.removeItem(i));
